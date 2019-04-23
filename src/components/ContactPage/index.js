b2f795e65ed9bada6be41ffa99b330431037b3e7
mitchellphilmore <mitchellphilmore@gmail.com>
2019-04-23 17:36:10 -0400

    removed images

--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -1,18 +1,11 @@
 import React, { useState } from "react";
 import "./index.css";
 import Navbar from "../Navbar";
-import img1 from "../driving1.jpg";
-import img2 from "../driving2.jpg";
-import img3 from "../driving3.jpg";
-import img4 from "../driving4.jpg";
-import img5 from "../driving5.jpg";
-import img6 from "../driving6.jpg";
-import img7 from "../driving7.jpg";
 import map from "../map.png";
 import ContactSection from "./ContactSection";
 import contactInfo from "./contactInfo";
 
-let images = [map, img3, img6];
+let images = [map];
 
 const ContactPage = () => {
   return (
