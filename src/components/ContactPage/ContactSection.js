3d4ab0da56288f091e70db9494e54b17007267cd
mitchellphilmore <mitchellphilmore@gmail.com>
2019-04-23 17:10:39 -0400

    formatted code

--- src/components/ContactPage/ContactSection.js
+++ src/components/ContactPage/ContactSection.js
@@ -1,35 +1,20 @@
-import React from 'react'
+import React from "react";
 
-
-const ContactSection = ({title,text,img})=>{
-    return(
-       <>
-       <div className="container">
+const ContactSection = ({ title, text, img }) => {
+  return (
+    <>
+      <div className="container">
         <div className="left">
           <span className="header animated zoomInUp">{title}</span>
           <p className="description animated zoomIn">{text}</p>
-        
         </div>
-       
+
         <div className="right" id="right">
-            <img className="images animated fadeInRight" src={img}/>
+          <img className="images animated fadeInRight" src={img} />
+        </div>
+      </div>
+    </>
+  );
+};
 
-           
-           
-          </div>
-    
-          </div>
-         
-         
-         
-        
-        </>
-        
-       
-        
-        
-    
-    )
-}
-
-export default ContactSection
+export default ContactSection;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,24 +1,40 @@
-import React from 'react'
-import './index.css'
-import logo from './starslogo.png'
+import React from "react";
+import "./index.css";
+import logo from "./starslogo.png";
 
+let Navbar = () => {
+  return (
+    <nav className="white">
+      <div class="nav-wrapper">
+        <a href="/" class="brand-logo black-text">
+          <img className="logo" src={logo} />
+          <p className="logoText"> Stars & Stripes Driving School</p>
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a className="black-text links" href="/">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="black-text links" href="/contact">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a className="black-text font links" href="">
+              Review
+            </a>
+          </li>
+          <li>
+            <a className="black-text links" href="/service">
+              Services
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-let Navbar = ()=>{
-     return (
-        <nav className="white">
-        <div class="nav-wrapper">
-          <a href="/" class="brand-logo black-text"><img className="logo" src={logo}/>
-           <p className="logoText"> Stars & Stripes Driving School</p></a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a className="black-text links" href="/">Home</a></li>
-            <li><a className="black-text links" href="/contact">Contact</a></li>
-            <li><a className="black-text font links" href="">Review</a></li>
-            <li><a className="black-text links" href="/service">Services</a></li>
-          </ul>
-        </div>
-      </nav>
-            
-     )
-}
-
-export default Navbar
+export default Navbar;
