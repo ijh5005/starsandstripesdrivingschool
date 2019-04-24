8b2a4b88cd6b6686a657dd5c1eb82d9ff10ac8ac
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2019-04-24 14:14:49 -0400

    reformat

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,25 +3,17 @@ import "./index.css";
 import logo from "./starslogo.png";
 
 let Navbar = () => {
-  return (
-    <nav className="white">
-<<<<<<< HEAD
-  <div className="topnav">
-  <a className="active" href="#home"> <img className="logo" src={logo}/></a>
-  <div className="linksright">
-=======
-  <div class="topnav">
-  <a class="active" href="/"> <img className="logo" src={logo}/></a>
-  <div class="linksright">
->>>>>>> 2cb4f8f614290e4a22699d0cc02beec51ed255c1
-    <a href="/">Home</a>
-    <a href="/contact">Contact</a>
-    <a href="/review">Reviews</a>
-    <a href="/service">Services</a>
-  </div>
-</div>
-</nav>
-  );
+  return (<nav className="white">
+    <div className="topnav">
+      <a className="active" href="#home"> <img className="logo" src={logo}/></a>
+      <div className="linksright">
+        <a href="/">Home</a>
+        <a href="/contact">Contact</a>
+        <a href="/review">Reviews</a>
+        <a href="/service">Services</a>
+      </div>
+    </div>
+  </nav>);
 };
 
 export default Navbar;
