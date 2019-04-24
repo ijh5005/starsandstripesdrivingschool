c8d9c55b1fe992344045104b2590942db9d584f2
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2019-04-23 21:35:13 -0400

    services page

--- src/components/ServicePage/index.js
+++ src/components/ServicePage/index.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import Navbar from '../Navbar';
 import Heading from './Heading';
+import PriceCard from './PriceCard';
 import "./index.css";
 
 const ServicePage = () => {
   return (<>
     <Navbar/>
     <Heading />
+    <PriceCard />
   </>)
 }
 
