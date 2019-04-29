e416112a59f08ae12e5616406843796a57d809f9
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2019-04-29 17:34:08 -0400

    contact fade in fix

--- src/components/ContactPage/ContactSection.js
+++ src/components/ContactPage/ContactSection.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NextBtn from "../NextBtn";
 
 const ContactSection = ({ title, text, img }) => {
+
+  useEffect(() => {
+    const bottom = document.getElementsByClassName('hideBeforeFadeInSetup')[0];
+    bottom.classList.remove("hideBeforeFadeIn");
+  }, [])
+
   return (
     <>
-      <div className="container">
+      <div className="container hideBeforeFadeIn hideBeforeFadeInSetup">
         <div className="left">
-          <span className="header animated zoomInUp">{title}</span>
-          <p className="description animated zoomIn">{text}</p>
+          <span className="header">{title}</span>
+          <p className="description">{text}</p>
         </div>
 
         <div
