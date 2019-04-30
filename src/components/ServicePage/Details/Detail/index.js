7073081287d2cb90318853a3980e63c91f298590
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2019-04-30 14:23:04 -0400

    service page update

--- src/components/ServicePage/Details/Detail/index.js
+++ src/components/ServicePage/Details/Detail/index.js
@@ -2,15 +2,13 @@ import React, { useState } from 'react';
 import "./index.css";
 
 const Detail = ({content, index}) => {
-
-  debugger
   const order = (index % 2 === 0);
-  const text = (<div className="detailSide">{content.text}</div>);
-  const img = (
-    <div class="detailSide">
-      <img className="detailImg" src={content.img}/>
-    </div>
-  )
+  const text = (<div className="detailSide">
+    <p>{content.text}</p>
+  </div>);
+  const img = (<div class="detailSide img">
+    <img className="detailImg" src={content.img}/>
+  </div>)
   return (<div className="detailComponent">
     {text}
     {img}
