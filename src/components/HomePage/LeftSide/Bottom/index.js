d38d2e6db19e2bba08f94c8d817f6dd9b9fe15ed
Isaiah Harrison <isaiahharrison@f0-18-98-15-42-a5.wireless.temple.edu>
2019-04-30 20:19:23 -0400

    changes

--- src/components/HomePage/LeftSide/Bottom/index.js
+++ src/components/HomePage/LeftSide/Bottom/index.js
@@ -12,6 +12,7 @@ const Bottom = () => {
     <div id="bottom" className="hideBeforeFadeIn hideBeforeFadeInSetup">
       <p id="title">Stars & Stripes Driving School</p>
       <p className="minitext">The best school in Philadelphia <span id="flag">&#9873;</span> <br/>Door to Door Lessons</p>
+      <p className="minitext">(267) 262 - 1196</p>
       <Link to={'/page/service'}>
         <div id="startBtn">
             <p>Start</p>
