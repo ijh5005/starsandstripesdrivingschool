39ee2748457d048f2e2cccdc47c70a5a5cde45b5
mitchellphilmore <mitchellphilmore@gmail.com>
2019-04-30 21:56:03 -0400

    added hours of operation section

--- src/components/ContactPage/contactInfo.js
+++ src/components/ContactPage/contactInfo.js
@@ -1,7 +1,9 @@
 const contactInfo = [
   {
     title: "CONTACT US TODAY",
-    text: " The Stars and Stripes driving school are ready to welcome all students who wish to learn to drive in all circumstance, driving in the day and driving at night in all the weather conditions. Rain and snow. Stars and Stripes Driving school open 7 days a week from 7:00am-8:00pm."
+    text: " The Stars and Stripes driving school are ready to welcome all students who wish to learn to drive in all circumstance, driving in the day and driving at night in all the weather conditions rain and snow. ",
+
+    schedule:'Stars and Stripes Driving school is open 7 days a week from 7:00am-8:00pm.'
   }
 ];
 
