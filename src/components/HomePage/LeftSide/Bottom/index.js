171d6df4520cf48a32ae2664f53525916c661eed
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2019-05-03 20:38:11 -0400

    phone link add and search text to home page

--- src/components/HomePage/LeftSide/Bottom/index.js
+++ src/components/HomePage/LeftSide/Bottom/index.js
@@ -12,10 +12,12 @@ const Bottom = () => {
     <div id="bottom" className="hideBeforeFadeIn hideBeforeFadeInSetup">
       <p id="title">Stars & Stripes Driving School</p>
       <p className="minitext">The best school in Philadelphia <span id="flag">&#9873;</span> <br/>Door to Door Lessons</p>
-      <p className="minitext">(267) 262 - 1196</p>
+      <a href="tel:8147530157">
+        <p className="minitext phoneLink">(267) 262 - 1196</p>
+      </a>
       <Link to={'/page/service'}>
         <div id="startBtn">
-            <p>Start</p>
+            <p>Search</p>
         </div>
       </Link>
     </div>
