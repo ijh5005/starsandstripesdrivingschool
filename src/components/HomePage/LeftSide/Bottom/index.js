2175268903967217f8863e447e35f849c25def9f
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2019-05-12 13:59:23 -0400

    number change

--- src/components/HomePage/LeftSide/Bottom/index.js
+++ src/components/HomePage/LeftSide/Bottom/index.js
@@ -12,7 +12,7 @@ const Bottom = () => {
     <div id="bottom" className="hideBeforeFadeIn hideBeforeFadeInSetup">
       <p id="title">Stars & Stripes Driving School</p>
       <p className="minitext">The best school in Philadelphia <span id="flag">&#9873;</span> <br/>Door to Door Lessons</p>
-      <a href="tel:8147530157">
+      <a href="tel:267-262-1196">
         <p className="minitext phoneLink">(267) 262 - 1196</p>
       </a>
       <Link to={'/page/service'}>
