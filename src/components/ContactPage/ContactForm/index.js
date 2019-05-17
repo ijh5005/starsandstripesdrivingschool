6fec75d3b856fc7cad5c8709baade2e931d24d9e
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2019-05-17 09:41:09 -0400

    contact form added

--- src/components/ContactPage/ContactForm/index.js
+++ src/components/ContactPage/ContactForm/index.js
@@ -1,22 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
 import './index.css'
 
 
-const ContactForm = ()=>{
-    return(
-        <>
-        <div class="form-style-8">
-  <h2>Got a question? We'd love to hear from you. Send us a message and we'll respond as soon as possible</h2>
-  <form>
-    <input type="text" name="field1" placeholder="Full Name" />
-    <input type="text" name="field2" placeholder="Phone number" />
-    <textarea placeholder="Message" onkeyup="adjust_textarea(this)"></textarea>
-    <input type="button" value="Send Message" />
-  </form>
-</div>
-      
-        </>
-    )
+const ContactForm = () => {
+
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [message, setMessage] = useState("");
+
+  const nameChange = (e) => {
+    const value = e.target.value;
+    setName(value)
+  }
+
+  const contactChange = (e) => {
+    const value = e.target.value;
+    setContact(value)
+  }
+
+  const messageChange = (e) => {
+    const value = e.target.value;
+    setMessage(value)
+  }
+
+  const submitEmail = (e) => {
+    e.preventDefault();
+    const params = {
+      name,
+      contact,
+      message
+    }
+    axios.post("https://blakioserver.herokuapp.com/mail", params)
+        .then(data => {
+          console.log(data);
+        }).catch(err => console.log(err));
+  }
+
+  return (<div id="contactForm">
+    <p className="smallText">Contact Us</p>
+    <div class="form-style-2">
+      <form action="" method="post">
+        <label for="field1">
+          <span>Name <span class="required">*</span></span>
+          <input type="text" class="input-field" name="field1" value={name} onChange={nameChange.bind(this)}/>
+        </label>
+        <label for="field2">
+          <span>Email <span class="required">*</span></span>
+          <input type="text" class="input-field" name="field2" value={contact} onChange={contactChange.bind(this)}/>
+        </label>
+        <label for="field5"><span>Message <span class="required">*</span></span>
+          <textarea name="field5" class="textarea-field" value={message} onChange={messageChange.bind(this)}></textarea></label>
+        <label>
+        <span> </span><input type="submit" value="Submit" onClick={submitEmail.bind(this)} /></label>
+      </form>
+    </div>
+  </div>)
 }
 
 export default ContactForm
--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
-import "./index.css";
+
 import Navbar from "../Navbar";
 import ContactSection from "./ContactSection";
-import contactInfo from "./contactInfo";
 import PrevBtn from "../PreviousBtn";
+import ContactForm from "./ContactForm"
+
+import "./index.css";
+import contactInfo from "./contactInfo";
 
 const ContactPage = () => {
-  return (
-    <>
-      {contactInfo.map((element, i) => (
-        <ContactSection
-          key={i}
-          title={element.title}
-          text={element.text}
-        />
-      ))}
-      <PrevBtn />
-    </>
-  );
+  return (<>
+    {contactInfo.map((element, i) => (
+      <ContactSection
+        key={i}
+        title={element.title}
+        text={element.text}
+      />
+    ))}
+    <ContactForm />
+    <PrevBtn />
+  </>);
 };
 
 export default ContactPage;
